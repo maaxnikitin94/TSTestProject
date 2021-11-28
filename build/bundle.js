38e5de4a0c6140349a4fc778db20c559390b051f
maaxnikitin94 <nikitinmr@gmail.com>
2021-11-28 20:48:14 +0300

    Edit: fix 0.1 {add names of blocks}

--- build/bundle.js
+++ build/bundle.js
@@ -1,1 +1,24 @@
+//DOM-элементы в переменные
+let Block1_elem = document.querySelector('.myBlock1');
+let Block2_elem = document.querySelector('.myBlock2');
+let Block3_elem = document.querySelector('.myBlock3');
+var MyBlocks;
+(function (MyBlocks) {
+    class TextEdit {
+        constructor(name, text) {
+            this.name = name;
+            this.text = text;
+        }
+        edit() {
+            this.name.innerHTML = this.text;
+        }
+    }
+    MyBlocks.TextEdit = TextEdit;
+})(MyBlocks || (MyBlocks = {}));
+let block1 = new MyBlocks.TextEdit(Block1_elem, 'BMW');
+let block2 = new MyBlocks.TextEdit(Block2_elem, 'AUDI');
+let block3 = new MyBlocks.TextEdit(Block3_elem, 'MERCEDES');
+block1.edit();
+block2.edit();
+block3.edit();
 //# sourceMappingURL=bundle.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
+//DOM-элементы в переменные
+let Block1_elem = document.querySelector('.myBlock1')
+let Block2_elem = document.querySelector('.myBlock2')
+let Block3_elem = document.querySelector('.myBlock3')
 
+
+namespace MyBlocks {
+
+    export class TextEdit {
+        constructor(public name:Element,public text:string) {
+        }
+
+        edit() {
+            this.name.innerHTML = this.text
+        }
+    }
+
+
+}
+let block1 = new MyBlocks.TextEdit(Block1_elem,'BMW')
+let block2 = new MyBlocks.TextEdit(Block2_elem,'AUDI')
+let block3 = new MyBlocks.TextEdit(Block3_elem,'MERCEDES')
+
+block1.edit()
+block2.edit()
+block3.edit()
